d3711538def7d4b297492e5557aa35d6a602b81f
Tanushree <shreetanu205@gmail.com>
2026-09-07 09:18:41 +0530

    Promise

---
+++ React/FSD_27/javascript/promise.js
@@ -0,0 +1,21 @@
+function f1(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("Hii!!")
+            resolve();
+        },4000)
+    })
+}
+function f2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("ABES")
+            resolve();
+        },2000)
+    })
+}
+
+f1().then(f2)
+    .catch((err)=>{
+        console.log("Error:",err);
+    })
